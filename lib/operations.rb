e720786e9ec5abf40c3d0c2f042e4ac3cd40f4ee
caitlin <caitlin.wink@gmail.com>
2021-06-29 11:06:55 -0700

    commit tree c1fbfe3e8edcc1ede7a0939fc5c48374552c50c8

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,18 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+    if (40..60) === speed
+        return false
+    else speed ==  0>40 || 0<60
+        return true
+    end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed ==  0>40 || 0<60 ? true : false 
+    (40..60) === speed ? false : true
 end
 	
 
